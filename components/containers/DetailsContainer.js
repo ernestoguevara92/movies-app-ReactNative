5d4099a2e948ef4b201853d047aef506b3614102
Roberto Guevara <robertoeguevara@gmail.com>
2022-10-05 11:39:40 -0700

    style details container

--- components/containers/DetailsContainer.js
+++ components/containers/DetailsContainer.js
@@ -1,17 +1,22 @@
-import {Box, Image, Heading, Text} from 'native-base';
+import {Box, Image, Heading, Text, VStack, HStack, Center} from 'native-base';
 import {IMAGE_URL} from '../../services/api_config';
 
 const DetailsContainer = ({navigation, route}) => {
     const { title, image, overview, popularity, releaseDate } = route.params
 
-    return (
-        <Box>
+    return (<Center>
+        <Box p={2}>
+            <VStack space={2} justifyContent="center" alignItems="center">
             <Heading size='lg'>{title}</Heading>
-            <Image alt={title} source={{uri: IMAGE_URL+image}} size="xl" resizeMode="cover" />
+            <Image alt={title} source={{uri: `https://image.tmdb.org/t/p/original${image}`}}  width='100%' size="2xl" resizeMode="cover"/>
             <Text>{overview}</Text>
-            <Text>Popularity: {popularity}</Text>
-            <Text>Release Date: {releaseDate}</Text>
+            <HStack>
+            <Text p={2} fontWeight='bold' >Popularity: {popularity}</Text>
+            <Text p={2} fontWeight='bold'>Release Date: {releaseDate}</Text>
+            </HStack>
+            </VStack>
         </Box> 
+    </Center>
     )
 }
 
